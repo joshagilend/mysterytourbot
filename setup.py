9830bf72dc710e6b868250092026edaa6cd56092
Josh Stroud <josh@agilend.net>
2023-05-08 06:47:32 -0700

    Add RSS library import to setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
-Index = open("index.html", "w")
+import datetime 
+from rfeed import *
 
-html = """<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>mysterytourbot</title>
-  </head>
-  <body>
-    <h1>Hello world</h1>
-  </body>
-</html>"""
+# def generate_rss(source_url):
+#   feed = Feed()
+#   return feed.rss()
 
-Index.write(html)
-Index.close
+def build_html():
+  Index = open("index.html", "w")
+
+  html = """<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>mysterytourbot</title>
+    </head>
+    <body>
+      <h1>Hello world</h1>
+    </body>
+  </html>"""
+
+  Index.write(html)
+  Index.close
+
+build_html()
